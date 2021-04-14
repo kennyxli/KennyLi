03c318fcdecb7d76ac9a166aba32333baa1c8016
Kenny Li <kenny.wj.li@gmail.com>
2021-04-14 11:14:29 -0400

    working on about me section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,23 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if(this.scrollY > 1){
+        if(this.scrollY > 20){
             $('.navbar').addClass("sticky")
         }
         else{
             $('.navbar').removeClass("sticky")
         }
     })
+    $(window).scroll(function(){
+        if(this.scrollY > 100){
+            $('.image').addClass("slide")
+        }
+        else{
+            $('.image').removeClass("slide")
+        }
+    })
     $('.menu-button').click(function(){
         $('.navbar .menu').toggleClass("active")
-        $('.home').toggleClass("active")
+        $('.body').toggleClass("active")
         $('.menu-button i').toggleClass("active")
     })
 })

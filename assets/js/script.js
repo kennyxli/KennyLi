bbd669e8a507754aa50347d45999d759103b4f50
Kenny Li <kenny.wj.li@gmail.com>
2021-04-13 20:26:16 -0400

    working on responsiveness

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 1){
+            $('.navbar').addClass("sticky")
+        }
+        else{
+            $('.navbar').removeClass("sticky")
+        }
+    })
+    $('.menu-button').click(function(){
+        $('.navbar .menu').toggleClass("active")
+        $('.home').toggleClass("active")
+    })
+})
